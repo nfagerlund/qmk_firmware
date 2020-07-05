64816ad305bcb31609e9fa8908166b31de450428
Nick Fagerlund <nick.fagerlund@gmail.com>
2020-07-05 12:13:27 -0700

    Replace keypad enter with main return/enter

--- keyboards/ymdk_np21/keymaps/return-nums/keymap.c
+++ keyboards/ymdk_np21/keymaps/return-nums/keymap.c
@@ -16,8 +16,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
     KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
     KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-    KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-    KC_P0,   KC_DOT,  KC_PDOT, KC_PENT
+    KC_P1,   KC_P2,   KC_P3,   KC_ENT,
+    KC_P0,   KC_DOT,  KC_PDOT, KC_ENT
   ),
 
   [_BL] = LAYOUT_ortho_6x4(
