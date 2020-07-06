d3d4c7a0db2fca8efc7137f4460e6712c696666b
Nick Fagerlund <nick.fagerlund@gmail.com>
2020-07-05 22:01:30 -0700

    Configure new gamepad layout

--- keyboards/ymdk_np21/keymaps/gamepad_rev2/keymap.c
+++ keyboards/ymdk_np21/keymaps/gamepad_rev2/keymap.c
@@ -1,6 +1,6 @@
 #include QMK_KEYBOARD_H
 
-#define _NP 0
+#define _WASD 0
 #define _BL 1
 
 enum custom_keycodes {
@@ -11,22 +11,22 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Qwerty */
 
-  [_NP] = LAYOUT_ortho_6x4(
-    KC_ESC,  KC_TAB,  KC_BSPC, MO(_BL),
-    KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
-    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-    KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-    KC_P0,   KC_DOT,  KC_PDOT, KC_PENT
+  [_WASD] = LAYOUT_ortho_6x4(
+    KC_1,    KC_2,    KC_3,    MO(_BL),
+    KC_G,    KC_C,    KC_R,    KC_TAB,
+    KC_E,    KC_W,    KC_Q,    KC_LSHIFT,
+    KC_A,    KC_S,    KC_D,    KC_LSHIFT,
+    KC_F,    KC_X,    KC_V,    KC_LCTRL,
+    KC_SPC,  KC_Z,    KC_Z,    KC_LCTRL
   ),
 
   [_BL] = LAYOUT_ortho_6x4(
-    _______, _______, _______, _______,
-    _______, _______, _______, _______,
-    _______, BL_ON,   _______, BL_INC,
-    _______, BL_TOGG, _______, BL_INC,
-    _______, BL_OFF,  _______, BL_DEC,
-    BL_BRTG, _______, _______, BL_DEC
+    KC_ESC,  KC_H,    KC_LALT, _______,
+    BL_TOGG, BL_DEC,  BL_INC,  KC_GRAVE,
+    KC_7,    KC_8,    KC_9,    KC_BSPC,
+    KC_4,    KC_5,    KC_6,    KC_BSPC,
+    KC_1,    KC_2,    KC_3,    KC_ENT,
+    KC_0,    _______, _______, KC_ENT
   )
 
 };
