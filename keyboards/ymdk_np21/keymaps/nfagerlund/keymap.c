87d47885d33875dd3f04a7472f88c46f3acc6d17
Nick Fagerlund <nick.fagerlund@gmail.com>
2020-10-12 12:56:20 -0700

    [pad] add a note for later

--- keyboards/ymdk_np21/keymaps/nfagerlund/keymap.c
+++ keyboards/ymdk_np21/keymaps/nfagerlund/keymap.c
@@ -73,6 +73,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ENT,  _______, DF(NUMPAD), _______
     ),
 
+    // TODO: Make fn+enter toggle a middle layer with KC_PENT, and implement
+    // layer_state_set_user() to always turn that layer off when switching to
+    // wasd mode.
     [F_NP] = LAYOUT_ortho_6x4(
         _______, _______, _______,  _______,
         _______, _______, _______,  _______,
