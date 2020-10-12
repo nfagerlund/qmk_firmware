86d382feb6a5dc2df9d8b10120b3631983a1569e
Nick Fagerlund <nick.fagerlund@gmail.com>
2020-10-12 12:42:43 -0700

    [pad] Change layer names

--- keyboards/ymdk_np21/keymaps/nfagerlund/keymap.c
+++ keyboards/ymdk_np21/keymaps/nfagerlund/keymap.c
@@ -1,10 +1,10 @@
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-    _WASD,
-    _NP,
-    _F_WASD,
-    _F_NP,
+    WASD,
+    NUMPAD,
+    F_WASD,
+    F_NP,
 };
 
 // BTW, these shouldn't affect numpad mode because that uses like KC_P1 instead of KC_1.
@@ -46,8 +46,8 @@ combo_t key_combos[COMBO_COUNT] = {
 // };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_WASD] = LAYOUT_ortho_6x4(
-        KC_1,    KC_2,    KC_3,    MO(_F_WASD),
+    [WASD] = LAYOUT_ortho_6x4(
+        KC_1,    KC_2,    KC_3,    MO(F_WASD),
         KC_G,    KC_C,    KC_R,    KC_TAB,
         KC_E,    KC_W,    KC_Q,    KC_LSHIFT,
         KC_A,    KC_S,    KC_D,    KC_LSHIFT,
@@ -55,8 +55,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_SPC,  KC_Z,    KC_Z,    KC_LCTRL
     ),
 
-    [_NP] = LAYOUT_ortho_6x4(
-        KC_ESC,  KC_TAB,  KC_BSPC, MO(_F_NP),
+    [NUMPAD] = LAYOUT_ortho_6x4(
+        KC_ESC,  KC_TAB,  KC_BSPC, MO(F_NP),
         KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
         KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
         KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
@@ -64,22 +64,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P0,   KC_DOT,  KC_PDOT, KC_ENT
     ),
 
-    [_F_WASD] = LAYOUT_ortho_6x4(
+    [F_WASD] = LAYOUT_ortho_6x4(
         KC_ESC,  KC_H,    KC_LALT, _______,
         KC_0,    KC_I,    _______, KC_ENT,
         _______, _______, _______, KC_BSPC,
         BL_BRTG, _______, _______, KC_BSPC,
         BL_TOGG, BL_DEC,  BL_INC,  _______,
-        KC_ENT,  _______, DF(_NP), _______
+        KC_ENT,  _______, DF(NUMPAD), _______
     ),
 
-    [_F_NP] = LAYOUT_ortho_6x4(
-        _______, _______, _______,   _______,
-        _______, _______, _______,   _______,
-        _______, _______, _______,   _______,
-        BL_BRTG, _______, _______,   _______,
-        BL_TOGG, BL_DEC,  BL_INC,    KC_PENT,
-        _______, _______, DF(_WASD), KC_PENT
+    [F_NP] = LAYOUT_ortho_6x4(
+        _______, _______, _______,  _______,
+        _______, _______, _______,  _______,
+        _______, _______, _______,  _______,
+        BL_BRTG, _______, _______,  _______,
+        BL_TOGG, BL_DEC,  BL_INC,   KC_PENT,
+        _______, _______, DF(WASD), KC_PENT
     ),
 
 };
