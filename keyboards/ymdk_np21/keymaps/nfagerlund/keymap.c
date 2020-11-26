2ad58358bdfa6d43515a3d39d33c987e1f0b9516
Nick Fagerlund <nick.fagerlund@gmail.com>
2020-11-25 19:12:56 -0800

    [pad] PDOT = PDOT
    I must have inherited this from the default layout, but anyway: the NP21 has
    build options for using either all 1u keys (the "macropad" style), or some 2u
    keys (the "numpad" style). I have the numpad style, so using different codes in
    those double-1u slots makes no sense. And that would have been a weird choice
    anyway.

--- keyboards/ymdk_np21/keymaps/nfagerlund/keymap.c
+++ keyboards/ymdk_np21/keymaps/nfagerlund/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
         KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
         KC_P1,   KC_P2,   KC_P3,   KC_ENT,
-        KC_P0,   KC_DOT,  KC_PDOT, KC_ENT
+        KC_P0,   KC_PDOT, KC_PDOT, KC_ENT
     ),
 
     // If you want real "numpad enter" instead of a return key.
